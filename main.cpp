0a8971a280fa69cce3ba2b194bc07c1da3f6d5b2
somerandomguythatneedshelp <107365137+somerandomguythatneedshelp@users.noreply.github.com>
2024-06-16 18:50:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
-constexpr unsigned long long f(const unsigned long long x) {
-	return x == 0 || x == 1 ? 1 : x * f(x - 1);
+constexpr unsigned long long IWonderWhatThisDoes(const unsigned long long x) {
+	return x == 0 || x == 1 ? 1 : x * IWonderWhatThisDoes(x - 1);
 }
 
 int main() {
 	constexpr unsigned x = 10;
 
-	std::printf("%d is: %llu", x, f(x));
+	std::printf("%d is: %llu", x, IWonderWhatThisDoes(x));
 
-
+	// try and guess what the code does
 }
